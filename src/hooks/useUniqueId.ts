2295e093593a41ea88aed58653d7d31fc46cdfcd
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-28 18:15:24 +0300

    create hook useUniqueId

---
+++ src/hooks/useUniqueId.ts
@@ -0,0 +1,37 @@
+import { randomUUID } from 'crypto';
+
+import { isUndefined } from '@/src/utils/type-checks';
+
+/**
+ * Use this hook to generate UUID.
+ *
+ * @example
+ * const id = useUniqueId(generatedId => {
+ *   // Dont add callback if you want to
+ *   // get only UUID.
+ *
+ *   return `gallery-${generatedId}`;
+ * });
+ *
+ * @param transformer     callback function that transforms generated
+ *                        ID and returns string.
+ *
+ * @return {string}
+ */
+export const useUniqueId = (
+  transformer?: (generatedId: string) => string
+): string => {
+  /** Generate UUID. */
+  const id = randomUUID();
+
+  /**
+   * If transformer callback is defined, call it
+   * and return result of it.
+   */
+  if (!isUndefined(transformer)) {
+    return transformer(id);
+  }
+
+  /** Return only ID if transformer is undefined. */
+  return id;
+};
