a0b9069d9081b4a3a73220bfb6ae8b7a13726468
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-28 18:51:16 +0300

    Update appSettingsSlice.ts

--- src/redux/reducers/appSettingsSlice.ts
+++ src/redux/reducers/appSettingsSlice.ts
@@ -4,27 +4,20 @@ import type { ReduxAction } from '@/src/redux/types';
 
 export type AppSettings = {
   appVersion: string;
-  appName: string;
-  language: 'en' | 'ru';
 };
 
 const initialState: AppSettings = {
   appVersion: '0.0.0',
-  appName: 'React Vite Application',
-  language: 'en',
 };
 
 const appSettingsSlice = createSlice({
   name: 'appSettings',
   initialState,
   reducers: {
-    /** Change language with action. */
-    changeLang(state, action: ReduxAction<AppSettings['language']>) {
-      state.language = action.payload;
-    },
+    simpleAction(state, action: ReduxAction<number>) {},
   },
 });
 
 export default appSettingsSlice.reducer;
-export const { changeLang } = appSettingsSlice.actions;
+export const { simpleAction } = appSettingsSlice.actions;
 export const initialAppSettings = appSettingsSlice.getInitialState();
