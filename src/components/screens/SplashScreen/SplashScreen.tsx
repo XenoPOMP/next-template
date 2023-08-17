206bf8d993300e2d92db39e6971832ef13fc1c0f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-08-17 17:24:11 +0300

    update SplashScreen

--- src/components/screens/SplashScreen/SplashScreen.tsx
+++ src/components/screens/SplashScreen/SplashScreen.tsx
@@ -5,7 +5,7 @@ import styles from './SplashScreen.module.scss';
 import type { SplashScreenProps } from './SplashScreen.props';
 
 const SplashScreen: FC<SplashScreenProps> = ({}) => {
-  return <aside className={cn(styles.splashScreen)}></aside>;
+  return <aside className={cn(styles.splashScreen)}>Loading ...</aside>;
 };
 
 export default SplashScreen;
--- src/redux/ReduxProvider.tsx
+++ src/redux/ReduxProvider.tsx
@@ -3,11 +3,19 @@
 import { PropsWith } from '@xenopomp/advanced-types';
 import { FC } from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from '@/src/redux/index';
+import SplashScreen from '@/src/components/screens/SplashScreen/SplashScreen';
+import store, { persistor } from '@/src/redux/index';
 
 const ReduxProvider: FC<PropsWith<'children', {}>> = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<SplashScreen />} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default ReduxProvider;
