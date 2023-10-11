2e49c2400cbe1e53cf45ee5f8308de981dc88e5e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-10-11 16:10:45 +0300

    create layout for store with hydration tracking

--- src/zustand/stores/layouts/storeLayout.ts
+++ src/zustand/stores/layouts/storeLayout.ts
@@ -3,6 +3,11 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 
 import defaultStorage from '@/src/zustand/storages/default-storage';
 
+/**
+ * The most common Zustand store example.
+ *
+ * _No hydration tracking._
+ */
 const useSuperStore = create<{}>()(
   persist((set, get) => ({}), {
     name: 'your-storage-name',
