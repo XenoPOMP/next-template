b78ebfced00b47f2085d4fd608c8b578d26b5234
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-10-11 14:22:57 +0300

    delete any redux usage

--- src/components/layout/Providers/Providers.tsx
+++ src/components/layout/Providers/Providers.tsx
@@ -1,12 +1,10 @@
 import { PropsWith } from '@xenopomp/advanced-types';
 import { FC } from 'react';
 
-import ReduxProvider from '@/src/redux/ReduxProvider';
-
 import type { ProvidersProps } from './Providers.props';
 
 const Providers: FC<PropsWith<'children', ProvidersProps>> = ({ children }) => {
-  return <ReduxProvider>{children}</ReduxProvider>;
+  return <>{children}</>;
 };
 
 export default Providers;
--- src/redux/types.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Type for Redux action.
- *
- * **A** generic is payload type.
- */
-export type ReduxAction<A> = {
-  payload: A;
-  type: string;
-};
