01de168cca14a63af6577a8b1bf31dee20fbdca1
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-01-02 11:14:23 +0300

    create func that converts value in pixels to local em

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import cn from 'classnames';
+
 import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
+
+import styles from './main-page.module.scss';
 
 export default function Home() {
   return (
-    <main>
+    <main className={cn(styles.mainPage)}>
       <UiContainer>index page</UiContainer>
     </main>
   );
