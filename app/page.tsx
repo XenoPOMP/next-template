b11b12077daf03fed9901735a680371325d6e895
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-01-14 17:59:40 +0300

    add custom metadata to main page
    This is needed to cover generateMetadata with tests

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import cn from 'classnames';
+import { Metadata } from 'next';
 
 import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
+import { generateMetadata } from '@/src/utils/generateMetadata';
 
 import styles from './main-page.module.scss';
+
+export const metadata: Metadata = generateMetadata({
+  title: 'Main',
+});
 
 export default function Home() {
   return (
