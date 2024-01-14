71c48bdf0e38389149fa4d6317d72d47b22c8139
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-01-14 12:24:11 +0300

    write test for useBoolean

---
+++ __tests__/unit/hooks/use-boolean.test.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import { fireEvent, render, screen } from '@testing-library/react';
+import { FC } from 'react';
+import { describe, expect, test } from 'vitest';
+
+import useBoolean from '@/src/hooks/useBoolean';
+
+const UseBooleanTestComponent: FC<{ initialValue?: boolean }> = ({
+  initialValue,
+}) => {
+  const [localValue, toggleLocalValue, setLocalValue] = useBoolean(
+    initialValue ?? false
+  );
+
+  return (
+    <>
+      <h1>This is an component for testing useBoolean hook</h1>
+
+      <div id={'value-preview'}>Enabled: {localValue ? 'true' : 'false'}</div>
+
+      <button id={'value-toggler'} onClick={() => toggleLocalValue()}>
+        Toggle value
+      </button>
+
+      <button id={'value-changer-to-true'} onClick={() => setLocalValue(true)}>
+        Change to true
+      </button>
+
+      <button
+        id={'value-changer-to-false'}
+        onClick={() => setLocalValue(false)}
+      >
+        Change to false
+      </button>
+    </>
+  );
+};
+
+describe('useBoolean hook', () => {
+  /** Click buttons in array. */
+  const clickAll = <B extends HTMLElement = HTMLButtonElement>(
+    buttons: Array<B>
+  ) => {
+    for (let button of buttons) {
+      fireEvent.click(button);
+    }
+  };
+
+  /** Expect local state to be certain value. */
+  const expectStateToBe = (state: boolean) => {
+    expect(() =>
+      screen.getAllByText(`Enabled: ${state ? 'true' : 'false'}`)
+    ).not.toThrow();
+  };
+
+  test('Not throwing errors', () => {
+    expect(() => render(<UseBooleanTestComponent />)).not.toThrow();
+  });
+
+  test('Not providing default value', () => {
+    render(<UseBooleanTestComponent />);
+
+    expectStateToBe(false);
+  });
+
+  test('Default value', () => {
+    render(<UseBooleanTestComponent initialValue={false} />);
+
+    expectStateToBe(false);
+  });
+
+  test('Toggle value', () => {
+    render(<UseBooleanTestComponent initialValue={false} />);
+
+    const buttons = screen.getAllByText<HTMLButtonElement>(`Toggle value`);
+
+    clickAll(buttons);
+    expectStateToBe(true);
+
+    clickAll(buttons);
+    expectStateToBe(false);
+  });
+
+  test('Change value', () => {
+    render(<UseBooleanTestComponent initialValue={true} />);
+
+    const changeToTrueButtons =
+      screen.getAllByText<HTMLButtonElement>(`Change to true`);
+    const changeToFalseButtons =
+      screen.getAllByText<HTMLButtonElement>(`Change to false`);
+
+    clickAll(changeToTrueButtons);
+    expectStateToBe(true);
+
+    clickAll(changeToFalseButtons);
+    expectStateToBe(false);
+  });
+});
