4412df3144fbdaaa959bd75452f14c21277d16ee
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-18 13:25:41 +0300

    🐈‍⬛ test BackgroundImage

---
+++ __tests__/unit/components/background-image.test.tsx
@@ -0,0 +1,13 @@
+import { describe, expect, test } from 'vitest';
+
+import catImage from '@/__tests__/assets/images/cat.jpeg';
+import { expectToRender } from '@/__tests__/assets/utilities';
+import BackgroundImage from '@/src/components/ui/BackgroundImage/BackgroundImage';
+
+describe('BackgroundImage component', () => {
+  test('It renders', () => {
+    expectToRender(
+      <BackgroundImage src={catImage} alt={'Sus'} width={1920} height={1080} />
+    );
+  });
+});
