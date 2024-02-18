d5e796eb10007dbcd068f02e162bede7fa7534d1
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-18 13:11:31 +0300

    🧪 test generateStaticMetadata

---
+++ __tests__/unit/utilities/generate-metadata.test.tsx
@@ -0,0 +1,49 @@
+import { Metadata } from 'next';
+import { describe, expect, test } from 'vitest';
+
+import { expectToDeepEqual } from '@/__tests__/assets/utilities/expectToDeepEqual';
+import { generateStaticMetadata } from '@/src/utils/generateStaticMetadata';
+
+describe('generateStaticMetadata func', () => {
+  test('No robots config defined', () => {
+    const generated = generateStaticMetadata({});
+    const real: Metadata = {
+      robots: null,
+    };
+
+    expectToDeepEqual(generated, real);
+  });
+
+  test('Only noIndex provided', () => {
+    const generated = generateStaticMetadata({
+      robots: { noIndex: true },
+    });
+    const real: Metadata = {
+      robots: 'noindex',
+    };
+
+    expectToDeepEqual(generated, real);
+  });
+
+  test('Only noFollow provided', () => {
+    const generated = generateStaticMetadata({
+      robots: { noFollow: true },
+    });
+    const real: Metadata = {
+      robots: 'nofollow',
+    };
+
+    expectToDeepEqual(generated, real);
+  });
+
+  test('noFollow and noIndex', () => {
+    const generated = generateStaticMetadata({
+      robots: { noFollow: true, noIndex: true },
+    });
+    const real: Metadata = {
+      robots: 'noindex, nofollow',
+    };
+
+    expectToDeepEqual(generated, real);
+  });
+});
