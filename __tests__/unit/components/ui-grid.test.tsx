649ccd2043dcdac09b95344f3e3fe4c5e85a6d3d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-18 13:27:24 +0300

    📱 test UiGrid

---
+++ __tests__/unit/components/ui-grid.test.tsx
@@ -0,0 +1,10 @@
+import { describe, expect, test } from 'vitest';
+
+import { expectToRender } from '@/__tests__/assets/utilities';
+import UiGrid from '@/src/components/ui/UiGrid/UiGrid';
+
+describe('UiGrid component', () => {
+  test('It renders', () => {
+    expectToRender(<UiGrid />);
+  });
+});
