0c015e6ee4c400f88d17ecd5e50d021c03dad497
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-18 12:13:10 +0300

    🫧 reformat config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @ts-check
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
