90afa1b52c4b2f55b470b99f517a1c7dff81db6a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-20 20:56:40 +0300

    🧪 test handleFunctionalChildren func

---
+++ __tests__/unit/utilities/handleFunctionalChildren.test.tsx
@@ -0,0 +1,29 @@
+import { describe, expect, test } from 'vitest';
+
+import { expectToRender } from '@/__tests__/assets/utilities';
+import { handleFunctionalChildren } from '@/src/utils/react';
+
+describe('handleFunctionalChildren func', () => {
+  test('It returns ReactNode', () => {
+    expectToRender(<>{handleFunctionalChildren(<></>)}</>);
+
+    expectToRender(
+      <>
+        {handleFunctionalChildren(() => (
+          <></>
+        ))}
+      </>,
+    );
+
+    expectToRender(
+      <>
+        {handleFunctionalChildren<[disabled: boolean]>(
+          disabled => (
+            <>{disabled ? 'Disabled' : 'Enabled'}</>
+          ),
+          false,
+        )}
+      </>,
+    );
+  });
+});
