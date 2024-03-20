6f6011360f6d6a99bec3338e58c757ae91334a52
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-20 22:44:53 +0300

    🧪 test async hook

--- __tests__/unit/hooks/use-fetch-hook.test.tsx
+++ __tests__/unit/hooks/use-fetch-hook.test.tsx
@@ -1,24 +1,40 @@
+import { render, waitFor } from '@testing-library/react';
 import { FC } from 'react';
 import { describe, expect, test } from 'vitest';
 
 import { expectToRender } from '@/__tests__/assets/utilities';
 import { useFetch } from '@/src/hooks/useFetch';
 
-const TestComponent: FC = () => {
-  const { isLoading, data } = useFetch(async () =>
-    fetch('https://www.npmjs.com'),
-  );
+const TestComponent: FC<{ url?: string }> = ({
+  url = 'https://jsonplaceholder.typicode.com/users/1',
+}) => {
+  const { isLoading, data, error } = useFetch(async () => fetch(url));
 
-  return (
-    <>
-      {isLoading && <p>Loading</p>}
-      {!isLoading && <p>{JSON.stringify(data, null, 2)}</p>}
-    </>
-  );
+  if (isLoading) {
+    return <>Loading...</>;
+  }
+
+  if (error) {
+    return <>Error...</>;
+  }
+
+  return <>This is a data</>;
 };
 
 describe('useFetch hook', () => {
   test('It does not throw', () => {
     expectToRender(<TestComponent />);
+  });
+
+  test('Wait for fetching', async () => {
+    const { getByText } = render(<TestComponent />);
+
+    await waitFor(() => getByText('This is a data'));
+  });
+
+  test('Provide incorrect url', async () => {
+    const { getByText } = render(<TestComponent url={''} />);
+
+    await waitFor(() => getByText('Error...'));
   });
 });
