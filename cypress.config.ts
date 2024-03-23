e56e0c108560857cedec7891b274e1af0b7e57c9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-23 12:43:41 +0300

    🪲 fix cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  fixturesFolder: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'http://localhost:3000',
     specPattern: '__tests__/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
