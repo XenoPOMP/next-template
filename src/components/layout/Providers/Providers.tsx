0dc407354933764d7094e1d5ad2134a12e735b2c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-29 11:35:25 +0300

    ℹ️ update theme info

--- src/components/layout/Providers/Providers.tsx
+++ src/components/layout/Providers/Providers.tsx
@@ -2,10 +2,22 @@ import { type PropsWith } from '@xenopomp/advanced-types';
 import { ThemeProvider } from 'next-themes';
 import { type FC } from 'react';
 
+import { AppConstants } from '@/app/app.constants';
+
 import type { ProvidersProps } from './Providers.props';
 
 const Providers: FC<PropsWith<'children', ProvidersProps>> = ({ children }) => {
-  return <ThemeProvider attribute={'class'}>{children}</ThemeProvider>;
+  const { appName } = AppConstants;
+
+  return (
+    <ThemeProvider
+      attribute={'class'}
+      storageKey={`[${appName}]:theme`}
+      enableSystem
+    >
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
