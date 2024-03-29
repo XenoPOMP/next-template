a29a10326dacee0b963357d95023e501c04b02e4
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-29 11:13:13 +0300

    🗄️ add ThemeProvider into providers

--- src/components/layout/Providers/Providers.tsx
+++ src/components/layout/Providers/Providers.tsx
@@ -1,10 +1,11 @@
 import { type PropsWith } from '@xenopomp/advanced-types';
+import { ThemeProvider } from 'next-themes';
 import { type FC } from 'react';
 
 import type { ProvidersProps } from './Providers.props';
 
 const Providers: FC<PropsWith<'children', ProvidersProps>> = ({ children }) => {
-  return <>{children}</>;
+  return <ThemeProvider attribute={'class'}>{children}</ThemeProvider>;
 };
 
 export default Providers;
