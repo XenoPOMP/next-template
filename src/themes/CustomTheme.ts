20397c0a27f3d0ed5e6d89968486e1987b9b3a84
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-29 11:05:22 +0300

    🖼️ create theme files

---
+++ src/themes/CustomTheme.ts
@@ -0,0 +1,4 @@
+import { type lightTheme } from '@/src/themes';
+
+/** This type is based on default theme. */
+export type CustomTheme = typeof lightTheme;
---
+++ src/themes/dark.theme.ts
@@ -0,0 +1,10 @@
+import { type CustomTheme } from '@/src/themes';
+
+export const darkTheme: CustomTheme = {
+  colors: {
+    primary: {
+      bg: '#0d1117',
+      font: '#FFF',
+    },
+  },
+};
