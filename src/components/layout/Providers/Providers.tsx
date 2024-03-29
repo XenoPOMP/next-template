61741673dd04ea12a17b9f9252f6de0844efbb83
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-29 12:45:34 +0300

    🗄️ start using func for generating storage keys

--- src/components/layout/Providers/Providers.tsx
+++ src/components/layout/Providers/Providers.tsx
@@ -2,17 +2,15 @@ import { type PropsWith } from '@xenopomp/advanced-types';
 import { ThemeProvider } from 'next-themes';
 import { type FC } from 'react';
 
-import { AppConstants } from '@/app/app.constants';
+import { createStorageKey } from '@/src/utils/misc';
 
 import type { ProvidersProps } from './Providers.props';
 
 const Providers: FC<PropsWith<'children', ProvidersProps>> = ({ children }) => {
-  const { appName } = AppConstants;
-
   return (
     <ThemeProvider
       attribute={'class'}
-      storageKey={`[${appName}]:theme`}
+      storageKey={createStorageKey('theme')}
       enableSystem
     >
       {children}
