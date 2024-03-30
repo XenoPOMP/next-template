a4f34f4be3d121a0e70b27d1798196ceb92cd4bb
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-30 13:23:06 +0300

    🚓 change paths to OG images

--- app/app.constants.ts
+++ app/app.constants.ts
@@ -1,3 +1,5 @@
+import ogSquareImg from '@/public/open-graph/og (200x200).png';
+import ogWideImg from '@/public/open-graph/og (1200x627).png';
 import type IAppConstants from '@/src/interfaces/AppConstants.interface';
 
 export const AppConstants: IAppConstants = {
@@ -6,12 +8,12 @@ export const AppConstants: IAppConstants = {
   sharedOpenGraphConfig: {
     images: [
       {
-        url: '/open-graph/og (200x200).png',
+        url: ogSquareImg.src,
         width: 200,
         height: 200,
       },
       {
-        url: '/open-graph/og (1200x627).png',
+        url: ogWideImg.src,
         width: 1200,
         height: 627,
       },
