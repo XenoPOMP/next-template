4aea7b827ce3a754c631c589216ee64a31ba45d6
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-31 13:48:47 +0300

    📏 shorten code

--- __tests__/assets/mocks/mock-fonts.ts
+++ __tests__/assets/mocks/mock-fonts.ts
@@ -14,10 +14,12 @@ const MockFont: NextFont = {
 export const mockFonts = () => {
   vi.mock('next/font/google', () => {
     const fontsToMock = ['Inter'];
-    const map: Record<string, () => NextFont> = {};
 
     // Bind mock font to each font name
-    fontsToMock.forEach(font => (map[font] = () => MockFont));
+    const map: Record<string, () => NextFont> = fontsToMock.reduce(
+      (o, key) => ({ ...o, [key]: () => MockFont }),
+      {},
+    );
 
     return map;
   });
