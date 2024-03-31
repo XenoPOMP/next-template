ba2fe270981239f7969e20a1f5a9f443880c343c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-31 15:10:35 +0300

    🧪 stub global variable

--- __tests__/unit/utilities/seo.test.tsx
+++ __tests__/unit/utilities/seo.test.tsx
@@ -1,10 +1,23 @@
-import { describe, expect, test } from 'vitest';
+import { afterEach, describe, test, vi } from 'vitest';
 
-import { testObject } from '@/__tests__/assets/utilities';
+import { stubGlobal, testObject } from '@/__tests__/assets/utilities';
+import { AppConstants } from '@/app/app.constants';
+import { generateOpenGraph } from '@/src/utils/seo';
 import { Months } from '@/src/utils/seo/sitemap-utils';
 
 describe('SEO utilities', () => {
+  afterEach(() => {
+    vi.unstubAllGlobals();
+  });
+
   test('Test Months enum', () => {
     testObject(Months);
+  });
+
+  test('Generate OG image', () => {
+    stubGlobal<typeof AppConstants>('AppConstants', {
+      sharedOpenGraphConfig: { images: undefined },
+    });
+    testObject(generateOpenGraph());
   });
 });
