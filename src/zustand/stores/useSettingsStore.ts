5ce54e2695caa0cf2e3f9d21c509f636a4aa936d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-31 12:10:01 +0300

    📈 create function for creating bound store

--- src/zustand/stores/useSettingsStore.ts
+++ src/zustand/stores/useSettingsStore.ts
@@ -1,24 +1,19 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
-
 import { createStorageKey } from '@/src/utils/misc';
+import { createBoundStore } from '@/src/zustand/utils';
 
 interface SettingsStore {
   lang: 'ru' | 'en';
   changeLang: (newLang: SettingsStore['lang']) => void;
 }
 
-const useSettingsStore = create<SettingsStore>()(
-  persist(
-    set => ({
-      lang: 'en',
-      changeLang: newLang => set({ lang: newLang }),
-    }),
-    {
-      name: createStorageKey('persist', 'settings'),
-      skipHydration: true,
-    },
-  ),
+const useSettingsStore = createBoundStore<SettingsStore>(
+  set => ({
+    lang: 'en',
+    changeLang: newLang => set({ lang: newLang }),
+  }),
+  {
+    name: createStorageKey('persist', 'settings'),
+  },
 );
 
 export default useSettingsStore;
