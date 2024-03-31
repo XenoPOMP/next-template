efede684ac138d15cde41d495647631a387d05ce
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-31 11:35:23 +0300

    🆕 update settings store

--- src/zustand/useSettingsStore.ts
+++ src/zustand/useSettingsStore.ts
@@ -3,12 +3,21 @@ import { persist } from 'zustand/middleware';
 
 import { createStorageKey } from '@/src/utils/misc';
 
-interface SettingsStore {}
+interface SettingsStore {
+  lang: 'ru' | 'en';
+  changeLang: (newLang: SettingsStore['lang']) => void;
+}
 
 const useSettingsStore = create<SettingsStore>()(
-  persist(() => ({}), {
-    name: createStorageKey('persist', 'settings'),
-  }),
+  persist(
+    set => ({
+      lang: 'en',
+      changeLang: newLang => set({ lang: newLang }),
+    }),
+    {
+      name: createStorageKey('persist', 'settings'),
+    },
+  ),
 );
 
 export default useSettingsStore;
