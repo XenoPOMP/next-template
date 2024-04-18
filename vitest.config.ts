c41fb710bf9cfff109b09356a2cde49ccc6a4c1c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-18 12:33:51 +0300

    🪲 fix Vitest testing exclusion

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,27 +2,40 @@ import react from '@vitejs/plugin-react';
 import tsconfigPathsPlugin from 'vite-tsconfig-paths';
 import { configDefaults, defineConfig } from 'vitest/config';
 
+const testingExclusion: string[] = [
+  // Default exclusions
+  ...configDefaults.exclude,
+
+  // Libraries & frameworks
+  '**/.next',
+
+  // Storybook
+  '.storybook',
+  '**/*.stories.{ts,tsx}',
+
+  // Testing artifacts
+  './coverage',
+  './app/test',
+  '__tests__/{e2e,assets}',
+
+  // Resource configs
+  '.*rc',
+  '.*rc.js',
+
+  // Ignore configs
+  '**/{next,postcss,tailwind}.config.{ts,js}',
+];
+
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tsconfigPathsPlugin()],
   test: {
     environment: 'jsdom',
+    exclude: testingExclusion,
     coverage: {
       provider: 'istanbul',
       reporter: ['text', 'json', 'json-summary', 'html'],
+      exclude: testingExclusion,
     },
-    exclude: [
-      // Default exclusions
-      ...configDefaults.exclude,
-
-      'cypress',
-      'node_modules',
-      'app/test',
-
-      // Ignore configs
-      'next.config.js',
-      'postcss.config.js',
-      'tailwind.config.ts',
-    ],
   },
   define: mode === 'test' ? {} : { global: 'window' },
 }));
