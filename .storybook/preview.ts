f91f86d7a0aca5a447fe5ecc76a6997d048ca77e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-18 12:42:10 +0300

    🌿 init themes addon

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,8 @@ import type { Preview } from '@storybook/react';
 
 import '../app/globals.scss';
 
+import { withThemeByClassName } from "@storybook/addon-themes";
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -11,6 +13,15 @@ const preview: Preview = {
       },
     },
   },
+
+  decorators: [withThemeByClassName({
+      themes: {
+          // nameOfTheme: 'classNameForTheme',
+          light: '',
+          dark: 'dark',
+      },
+      defaultTheme: 'light',
+  })]
 };
 
 export default preview;
