bf3c11361362b8d063e58cad228e441be7954d7f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-07-20 17:12:43 +0300

    📝 refactor hook imports

--- src/hooks/useOutSide.ts
+++ src/hooks/useOutSide.ts
@@ -1,6 +1,12 @@
 import { useOutside } from '@pacote/react-use-outside';
 import { useState } from 'react';
 
+/**
+ * Detects clicks outside ref element and changes
+ * state according to it.
+ *
+ * Returns state and ref.
+ */
 export const useOutSide = <Ref extends HTMLElement>() => {
   const [isShown, setIsShown] = useState<boolean>(false);
   const ref = useOutside<Ref>('click', () => setIsShown(false));
