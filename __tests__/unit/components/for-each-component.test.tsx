180fab756dc6e9c5d442a2c81efd37e147a7a1f3
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-10 14:25:28 +0300

    🧪 write test for "For" component

---
+++ __tests__/unit/components/for-each-component.test.tsx
@@ -0,0 +1,51 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+
+import { expectToRender } from '@/__tests__/assets/utilities';
+import { For } from '@/src/components/layout';
+
+describe('"For" component tests', () => {
+  afterEach(() => cleanup());
+
+  test('It renders', () => {
+    expectToRender(<For data={[]} />);
+  });
+
+  test('Internal .map functionality works', () => {
+    const testId = `value-holder`;
+
+    render(
+      <For
+        data={[
+          {
+            value: 2,
+          },
+          {
+            value: 4,
+          },
+          {
+            value: 8,
+          },
+        ]}
+      >
+        {({ value }, index) => (
+          <div
+            data-testid={testId}
+            data-value={value * 2}
+            key={`test-item-${index}`}
+          >
+            Dummy
+          </div>
+        )}
+      </For>,
+    );
+
+    const components = screen.getAllByTestId<HTMLDivElement>(testId);
+    const values = components
+      .map(component => component.getAttribute('data-value'))
+      .filter(value => value !== null)
+      .map(value => +value);
+
+    expect(values).toStrictEqual([4, 8, 16]);
+  });
+});
