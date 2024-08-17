4bb6277c20044185eb37a8508e20e82ae495b5dd
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-17 18:10:52 +0300

    ✍️ partial code separation

--- src/interfaces/AppConstants.interface.ts
+++ src/interfaces/AppConstants.interface.ts
@@ -1,20 +1,4 @@
-import type { Defined, Modify } from '@xenopomp/advanced-types';
-import { type Metadata } from 'next';
-
-/**
- * Config of Open Graph from Next Metadata.
- *
- * @see [Next Metadata docs](https://nextjs.org/docs/app/building-your-application/optimizing/metadata#merging)
- */
-type OpenGraphConfig = Defined<NonNullable<Metadata['openGraph']>>;
-
-export type OGImage = Defined<Exclude<OpenGraphConfig['images'], Array<any>>>;
-
-export type ModifiedOGConfig = Modify<
-  OpenGraphConfig,
-  'images',
-  Array<OGImage>
->;
+import type { ModifiedOGConfig } from '@/src/types';
 
 interface IAppConstants {
   appName: string;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,2 @@
 export * from './InferBoundStoreType';
+export * from './open-graph';
