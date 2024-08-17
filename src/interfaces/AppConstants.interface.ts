dd97f113b740dd586f9009af4a433a8d22a51712
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-17 18:21:59 +0300

    🗑️ delete interfaces folder

--- src/interfaces/AppConstants.interface.ts
+++
@@ -1,9 +0,0 @@
-import type { ModifiedOGConfig } from '@/src/types';
-
-interface IAppConstants {
-  APP_NAME: string;
-  defaultCanonical: string;
-  SHARED_OG_CONFIG: Partial<ModifiedOGConfig>;
-}
-
-export default IAppConstants;
