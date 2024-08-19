0d69bc7bc98a2ed84c9037806fc7296943092970
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-19 16:53:54 +0300

    📝 change env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,10 +1,8 @@
 import { z } from 'zod';
 
-import { DEFAULT_CANONICAL } from '@/app/constants';
-
 // Schema of env parsed object
 const envSchema = z.object({
-  CANONICAL_URL: z.string().default(DEFAULT_CANONICAL),
+  CANONICAL_URL: z.string(),
 });
 
 // Use this object to validate env file
