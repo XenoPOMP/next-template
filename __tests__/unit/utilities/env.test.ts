eae2e89338c76fad5d2dd4a3fc10c7badbe739b0
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-19 17:13:19 +0300

    🧪 update test for env

--- __tests__/unit/utilities/env.test.ts
+++ __tests__/unit/utilities/env.test.ts
@@ -1,13 +1,28 @@
-import { describe, expect, test } from 'vitest';
+import { afterAll, describe, expect, test } from 'vitest';
 
+import { clearMocks, mockEnv } from '@/__tests__/assets/mocks';
 import { env } from '@/src/utils/env';
 
 describe('env with zod schema tests', () => {
+  afterAll(() => {
+    clearMocks();
+  });
+
   test('Default canonical url is set', () => {
     expect(env.CANONICAL_URL).toBe('http://localhost:3000');
   });
 
   test('Default production mode is set', () => {
     expect(env.NEXT_PUBLIC_PRODUCTION_MODE).toBe('dev');
+  });
+
+  test('Production mode is parsed only with allowed values', () => {
+    mockEnv({
+      NEXT_PUBLIC_PRODUCTION_MODE: 'someThingElse',
+    });
+
+    expect(env.NEXT_PUBLIC_PRODUCTION_MODE).toBe('dev');
+
+    clearMocks();
   });
 });
