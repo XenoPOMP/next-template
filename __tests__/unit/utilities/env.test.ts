17445a4062116743b2e212482b1849e32b280aa8
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-19 17:09:05 +0300

    ⚙️ setup production mode

--- __tests__/unit/utilities/env.test.ts
+++ __tests__/unit/utilities/env.test.ts
@@ -6,4 +6,8 @@ describe('env with zod schema tests', () => {
   test('Default canonical url is set', () => {
     expect(env.CANONICAL_URL).toBe('http://localhost:3000');
   });
+
+  test('Default production mode is set', () => {
+    expect(env.NEXT_PUBLIC_PRODUCTION_MODE).toBe('dev');
+  });
 });
