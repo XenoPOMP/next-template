0a9a4ea56fc1c1032733226e4e9ea1596212f6bb
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-10-25 18:22:02 +0300

    📏 update config lib

--- .config/eslint.config.mjs
+++ .config/eslint.config.mjs
@@ -1,19 +1,3 @@
 import xenopomp from 'eslint-config-xeno';
 
-export default xenopomp(
-  {},
-  {
-    name: 'Rules breakup #2',
-    rules: {
-      'style/operator-linebreak': 'off',
-      'test/consistent-test-it': 'off',
-      'test/prefer-lowercase-title': 'off',
-      'style/jsx-quotes': 'off',
-      'style/multiline-ternary': 'off',
-      'style/indent': 'off',
-    },
-    ignores: [
-      'cypress',
-    ],
-  },
-);
+export default xenopomp();
