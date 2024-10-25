085443a9df8a46780889f3c2a9c927ee29f6854b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-10-25 16:05:38 +0300

    ⚙️ modify eslint config

--- .config/eslint.config.mjs
+++ .config/eslint.config.mjs
@@ -1,3 +1,11 @@
 import xenopomp from 'eslint-config-xeno';
 
-export default xenopomp();
+export default xenopomp(
+  {},
+  {
+    name: 'Rules breakup #2',
+    rules: {
+      'style/operator-linebreak': 'off',
+    },
+  },
+);
