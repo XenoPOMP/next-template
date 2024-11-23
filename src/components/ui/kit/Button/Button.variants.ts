447e1434ba5eb24c5bf88460d87f6b09dd078881
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-11-23 12:45:15 +0200

    🔳 update default button variant styles

--- src/components/ui/kit/Button/Button.variants.ts
+++ src/components/ui/kit/Button/Button.variants.ts
@@ -2,7 +2,7 @@ import { type VariantProps, cva } from 'class-variance-authority';
 
 export type ButtonVariantsType = VariantProps<typeof buttonVariants>;
 
-export const buttonVariants = cva('', {
+export const buttonVariants = cva('inline-flex', {
   variants: {
     variant: {
       default: 'TODO',
