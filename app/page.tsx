70f60ed10049dd9f4eb4c327d5ef17c60fe0036c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-12-31 16:12:45 +0200

    🫙 change UiContainer to Stack

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import cn from 'classnames';
 import type { Metadata } from 'next';
 
-import { UiContainer } from '@/src/components/ui';
+import { Stack } from '@/src/components/ui';
 import { generateStaticMetadata } from '@/src/utils/seo';
 
 import styles from './main-page.module.scss';
@@ -13,7 +13,7 @@ export const metadata: Metadata = generateStaticMetadata({
 export default function Home() {
   return (
     <main className={cn(styles.mainPage)}>
-      <UiContainer>index page</UiContainer>
+      <Stack>index page</Stack>
     </main>
   );
 }
