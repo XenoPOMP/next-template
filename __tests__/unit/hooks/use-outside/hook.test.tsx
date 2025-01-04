936e720e5ee45d95a43efb6f16467a0673e87acd
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-01-04 22:47:12 +0200

    🧼 change deprecated hook

--- __tests__/unit/hooks/use-outside/hook.test.tsx
+++ __tests__/unit/hooks/use-outside/hook.test.tsx
@@ -1,10 +1,30 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import type { ComponentRef } from 'react';
 import { describe, test } from 'vitest';
 
+import { assertNotThrowing } from '@/__tests__/assets/assertions';
 import { expectHookToRender } from '@/__tests__/assets/utilities';
 import { useOutSide } from '@/src/hooks';
+
+function Testing() {
+  const { ref } = useOutSide<ComponentRef<'div'>>();
+
+  return (
+    <div>
+      <div data-testid='out'>Outside</div>
+      <div ref={ref}>Ref</div>
+    </div>
+  );
+}
 
 describe('useOutside hook', () => {
   test('It renders', () => {
     expectHookToRender(() => useOutSide());
+  });
+
+  test('Outside click', () => {
+    render(<Testing />);
+    const outside = screen.getByTestId('out');
+    assertNotThrowing(() => fireEvent.click(outside));
   });
 });
--- src/hooks/useCopyToClipboard.ts
+++ src/hooks/useCopyToClipboard.ts
@@ -1,44 +1,12 @@
 'use client';
 
-import { useCallback, useEffect, useState } from 'react';
+import { useClipboard } from 'use-clipboard-copy';
 
 /**
  * This hook allows copy text to clipboard.
  *
- * **Not working inside server components!!!**
+ * @deprecated Use **useClipboard** from `'use-clipboard-copy'` instead
  */
-const useCopyToClipboard = (): {
-  isCopied: boolean;
-  copy: (text: string) => void;
-} => {
-  const [isCopied, setIsCopied] = useState(false);
-
-  const copy = useCallback((text: string) => {
-    navigator.clipboard
-      .writeText(text)
-      .then(() => {
-        setIsCopied(true);
-      })
-      .catch(err => {
-        console.error(`Unable to copy text: ${err}`);
-      });
-  }, []);
-
-  useEffect(() => {
-    if (isCopied) {
-      const timer = setTimeout(() => setIsCopied(false), 3000);
-
-      return () => clearTimeout(timer);
-    }
-
-    // eslint-disable-next-line no-useless-return
-    return;
-  }, [isCopied]);
-
-  return {
-    isCopied,
-    copy,
-  };
-};
+const useCopyToClipboard = useClipboard;
 
 export default useCopyToClipboard;
