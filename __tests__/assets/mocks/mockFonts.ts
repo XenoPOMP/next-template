f503b7a9fdaef296e8bf6381842ba4e1debd2579
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-01 20:49:42 +0300

    🧪 refactor mockFonts

--- __tests__/assets/mocks/mockFonts.ts
+++ __tests__/assets/mocks/mockFonts.ts
@@ -3,23 +3,22 @@ import { vi } from 'vitest';
 
 import { FONTS_TO_MOCK } from '@app/constants';
 
-/** Primitive font mock. */
-const MockFont: NextFont = {
-  className: 'className',
-  style: {
-    fontFamily: 'fontFamily',
-  },
-};
+// Bind mock font to each font name
+const FONT_MAP: Record<string, () => NextFont> = FONTS_TO_MOCK.reduce(
+  (o, key) => ({
+    ...o,
+    [key]: () => ({
+      className: 'className',
+      style: {
+        fontFamily: 'fontFamily',
+      },
+    }),
+  }),
+  {},
+);
 
 /** This function allows you to mock fonts from **next/google/fonts**. */
 export const mockFonts = () => {
-  vi.mock('next/font/google', () => {
-    // Bind mock font to each font name
-    const map: Record<string, () => NextFont> = FONTS_TO_MOCK.reduce(
-      (o, key) => ({ ...o, [key]: () => MockFont }),
-      {},
-    );
-
-    return map;
-  });
+  vi.mock('next/font/google', () => FONT_MAP);
+  vi.mock('next/dist/compiled/@next/font/dist/google', () => FONT_MAP);
 };
