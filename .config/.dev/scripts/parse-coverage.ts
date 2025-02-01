39c373d4016361cf6f5a7f1f865e0819af5891bf
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-01 22:11:31 +0300

    📜 update script

--- .config/.dev/scripts/parse-coverage.ts
+++ .config/.dev/scripts/parse-coverage.ts
@@ -1,16 +1,17 @@
 import * as cheerio from 'cheerio';
 import { ElementType } from 'domelementtype';
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 import path from 'node:path';
 
 const pathToHtml = path.join(process.cwd(), 'coverage/index.html');
+const pathToReadme = path.join(process.cwd(), 'README.md');
 
-(async () => {
-  const content = await readFile(pathToHtml, {
+const getAvgCoverage = async () => {
+  const htmlContent = await readFile(pathToHtml, {
     encoding: 'utf-8',
   });
 
-  const $ = cheerio.load(content);
+  const $ = cheerio.load(htmlContent);
 
   const percents = $('body .wrapper .clearfix .fl span.strong')
     .toArray()
@@ -23,5 +24,46 @@ const pathToHtml = path.join(process.cwd(), 'coverage/index.html');
 
   const sum = percents.reduce((acc, next) => acc + next, 0);
   const length = percents.length;
-  const avg = sum / length;
+  return sum / length;
+};
+
+const writeToReadme = async (avg: number) => {
+  const readmeContent = await readFile(pathToReadme, {
+    encoding: 'utf-8',
+  });
+
+  let color: string = '';
+
+  if (avg === 100) {
+    color = 'darkgreen';
+  }
+
+  if (avg < 100) {
+    color = '#05e01e';
+  }
+
+  if (avg < 90) {
+    color = '#ffa600';
+  }
+
+  if (avg < 50) {
+    color = '#f00';
+  }
+
+  // Normalize color
+  color = color.replace(/#/g, '');
+
+  const regex =
+    /<img alt="Coverage status" src="https:\/\/img\.shields\.io\/badge.*>/g;
+  const replaced = readmeContent.replace(
+    regex,
+    `<img alt="Coverage status" src="https://img.shields.io/badge/coverage-${avg.toFixed(0)}%25-${color}">`,
+  );
+
+  await writeFile(pathToReadme, replaced);
+};
+
+(async () => {
+  const avg = await getAvgCoverage();
+  await writeToReadme(100);
 })();
