87d63eda32a93c6af47fc925124b6d05d71b8d0a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-05 23:57:59 +0200

    🧪 update hook test

--- __tests__/unit/zustand/example-store.test.tsx
+++ __tests__/unit/zustand/example-store.test.tsx
@@ -1,11 +1,22 @@
-import { describe, test } from 'vitest';
+import { renderHook } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
 
 import { DEFAULT_SELECTOR, useExampleStore } from '@/zustand';
 
-import { expectHookToRender } from '@test/assets';
+import { assertNotThrowing, expectHookToRender } from '@test/assets';
 
 describe('useExampleStore hook', () => {
   test('It renders', () => {
     expectHookToRender(() => useExampleStore(DEFAULT_SELECTOR));
+  });
+
+  test('Setter function works as expected', () => {
+    const { result } = renderHook(() => useExampleStore(DEFAULT_SELECTOR), {
+      hydrate: true,
+    });
+    const { setText, text } = result.current;
+
+    assertNotThrowing(() => setText('12'));
+    expect(text).toBe('');
   });
 });
