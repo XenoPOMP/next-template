45117794af53873465b8bb13fc00ebfaeda59e63
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-08 11:41:13 +0200

    🚮 delete deprecated tests

--- __tests__/assets/utilities/index.ts
+++ __tests__/assets/utilities/index.ts
@@ -1,6 +1,4 @@
 export { testObject } from './testObject';
-export { booleanishString } from './booleanishString';
-export { clickAll } from './clickAll';
 export * from './expect-to-render';
 export * from './expectToDeepEqual';
 export * from './stubGlobal';
