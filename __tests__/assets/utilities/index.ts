2a2c4a0557133e75f0b054ba860216e51181ed3e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-08 13:05:54 +0200

    🚮 delete expectToDeepEqual

--- __tests__/assets/utilities/index.ts
+++ __tests__/assets/utilities/index.ts
@@ -1,6 +1,5 @@
 export { testObject } from './testObject';
 export * from './expect-to-render';
-export * from './expectToDeepEqual';
 export * from './stubGlobal';
 export * from './injectMocks.ts';
 export * from './testNextPage.ts';
