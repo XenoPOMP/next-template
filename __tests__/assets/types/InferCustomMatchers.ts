bb7329769d6eca933eb0f120972d9a122869c483
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-08 12:26:39 +0200

    🌀 fix typings

--- __tests__/assets/types/InferCustomMatchers.ts
+++ __tests__/assets/types/InferCustomMatchers.ts
@@ -1,6 +1,7 @@
-import type { MatcherState, RawMatcherFn } from '@vitest/expect';
+import type { MatcherState } from '@vitest/expect';
 import type { Fn } from 'xenopomp-essentials';
 
+import type { VitestMatcher } from '@test/assets';
 import type { matchers } from '@test/setup.vitest';
 
 type Matchers = typeof matchers & { sus: string };
@@ -8,10 +9,9 @@ type Matchers = typeof matchers & { sus: string };
 export type InferCustomMatchers<R extends MatcherState = any> = {
   [Matcher in keyof Matchers as Matchers[Matcher] extends Fn
     ? Matcher
-    : never]: Matchers[Matcher] extends Fn<
-    [actual: infer Actual, expected: any],
-    ReturnType<RawMatcherFn<R>>
-  >
-    ? (actual: Actual) => R
+    : never]: Matchers[Matcher] extends VitestMatcher<any, infer Expected>
+    ? [Expected] extends [unknown]
+      ? () => R
+      : (expected: Expected) => R
     : never;
 };
