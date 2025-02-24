f4be8c3277e364ed8d114cbfa41e08caefb4c076
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-24 22:44:54 +0200

    🤮 check if metadata wont throw

--- __tests__/assets/assertions/not-throwing.ts
+++ __tests__/assets/assertions/not-throwing.ts
@@ -1,6 +1,6 @@
 import { expect } from 'vitest';
 
 /** Executes function and expects that it won't throw. */
-export const assertNotThrowing = (callable: () => void) => {
+export const assertNotThrowing = (callable: () => void | Promise<void>) => {
   expect(() => callable()).not.toThrow();
 };
