ac8959ff141d811f12e0f972c1b635ea1f4455f6
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-02-24 22:00:44 +0200

    🗑️ delete DEFAULT_SELECTOR
    Idea by https://github.com/XenoPOMP/dir-tree-visualizer-app/commit/2950335f5813a13ffef4118e0b62e1961632ad0a

--- __tests__/unit/zustand/example-store.test.tsx
+++ __tests__/unit/zustand/example-store.test.tsx
@@ -1,17 +1,17 @@
 import { renderHook } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 
-import { DEFAULT_SELECTOR, useExampleStore } from '@/zustand';
+import { useExampleStore } from '@/zustand';
 
 import { assertHookRendering, assertNotThrowing } from '@test/assets';
 
 describe('useExampleStore hook', () => {
   test('It renders', () => {
-    assertHookRendering(() => useExampleStore(DEFAULT_SELECTOR));
+    assertHookRendering(() => useExampleStore());
   });
 
   test('Setter function works as expected', () => {
-    const { result } = renderHook(() => useExampleStore(DEFAULT_SELECTOR), {
+    const { result } = renderHook(() => useExampleStore(), {
       hydrate: true,
     });
     const { setText, text } = result.current;
--- src/zustand/defaults/index.ts
+++
@@ -1,2 +0,0 @@
-/**  */
-export const DEFAULT_SELECTOR = <T>(s: T) => s;
