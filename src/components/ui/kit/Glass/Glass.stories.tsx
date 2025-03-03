7d2ee043536de5e2233b94ce574c75cbb56af615
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-03 14:26:16 +0200

    💬 add new story

--- src/components/ui/kit/Glass/Glass.stories.tsx
+++ src/components/ui/kit/Glass/Glass.stories.tsx
@@ -20,6 +20,7 @@ const sharedProps = {
   children: <header>Glass header</header>,
   blur: 8,
   className: cn('py-[8px] sticky top-0'),
+  asChild: true,
 } satisfies Partial<Story['args']>;
 
 export const StickyHeader: Story = {
@@ -33,15 +34,38 @@ export const StickyHeader: Story = {
         <p>Content</p>
       ))}
 
-      <Glass
-        asChild
-        {...props}
-      />
+      <Glass {...props} />
 
       {Array.from({ length: 25 }, () => (
         // eslint-disable-next-line react/no-missing-key
         <p>Content</p>
       ))}
+    </div>
+  ),
+};
+
+export const GlassBlock: Story = {
+  args: {
+    ...sharedProps,
+  },
+  parameters: {
+    layout: 'centered',
+  },
+  render: props => (
+    <div className={cn('relative size-[100px]')}>
+      <Glass
+        {...props}
+        className={cn(
+          'absolute left-0 top-0 size-full',
+          'flex-center aspect-square rounded bg-white p-[8px]',
+          'z-20',
+        )}
+        blur={2}
+      />
+
+      <div className={cn('absolute left-0 top-0 size-full', 'z-10')}>
+        Backdrop
+      </div>
     </div>
   ),
 };
