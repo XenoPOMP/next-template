f097337534c6c77ab4476fa037d8bbfd865bbe8f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-06 11:55:38 +0200

    ℹ️ create cleanup script

--- .config/.dev/scripts/cleanup.ts
+++ .config/.dev/scripts/cleanup.ts
@@ -1,7 +1,11 @@
 import { bold } from 'ansi-colors';
+import type { PackageJson } from 'type-fest';
 import { z } from 'zod';
 
+import packageJson from '../../../package.json';
+
 import { DevLogger } from './logger.ts';
+import cwd from './utils/cwd.ts';
 
 const envSchema = z.object({
   ACTOR: z.string().optional(),
@@ -13,8 +17,23 @@ const env = envSchema.parse(process.env);
 (async () => {
   DevLogger.start('Starting cleanup process...');
 
-  DevLogger.info(`User: ${bold(env.REPO_NAME ?? 'none')}`);
+  DevLogger.info(`Repo: ${bold(env.REPO_NAME ?? 'none')}`);
   DevLogger.info(`User: ${bold(env.ACTOR ?? 'none')}`);
+
+  DevLogger.info(`Starting to cleaning at ${bold(cwd())}`);
+
+  const packageJsonContent = structuredClone<PackageJson>(packageJson);
+
+  // Update scripts
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  const { cleanup, ...scripts } = packageJsonContent.scripts ?? {};
+  packageJsonContent.scripts = scripts;
+
+  // Update author info
+  packageJsonContent.author = env.ACTOR;
+  packageJsonContent.name = env.REPO_NAME;
+  packageJsonContent.repository = `https://github.com/${env.ACTOR}/${env.REPO_NAME}`;
+  packageJsonContent.version = '0.0.0';
 
   DevLogger.end('Cleanup ended. ✨');
 })();
