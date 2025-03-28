de266d7a35ae52217f9daf7d00e68626d6563bb3
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-28 15:17:02 +0200

    💬 update comments

--- src/hooks/use-tracked-state/useTrackedState.ts
+++ src/hooks/use-tracked-state/useTrackedState.ts
@@ -9,7 +9,12 @@ type MountFn<S> = (updated: S) => void;
 type UseStateResult<S> = [S, SetState<S>];
 
 /**
+ * Works as useState hook, but allows to provide callback which
+ * will called on each non-first mount effect.
  *
+ * @example
+ * // Will log state each time it updates (but skips initial render).
+ * const [state, updateState] = useTrackedState(initialValue ?? 1, (v) => console.log(v));
  */
 export function useTrackedState<S>(
   initialState: S | Fn<[], S>,
