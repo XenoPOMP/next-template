f82fb8757579a5678511bad55411dc71a04c5b3f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-28 20:06:51 +0200

    📝 enhance schema parsing

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,10 +1,18 @@
 import { z } from 'zod';
 
+const lenientSchema = z.intersection(z.string(), z.object({}));
+
+const nodeEnvSchema = z
+  .union([z.literal('development'), z.literal('production'), lenientSchema])
+  .optional();
+
 // Schema of env parsed object
 const envSchema = z.object({
   CANONICAL_URL: z.string().default('http://localhost:3000'),
-  NODE_ENV: z.string().optional(),
+  NODE_ENV: nodeEnvSchema,
 });
+
+export type EnvSchema = z.infer<typeof envSchema>;
 
 // Use this object to validate env file
 export const env = envSchema.parse(process.env);
