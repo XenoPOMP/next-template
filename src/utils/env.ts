c12a5bba80ece4d948a300994a0965120160f2d6
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-28 20:02:07 +0200

    🐞 fix env parsing

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 // Schema of env parsed object
 const envSchema = z.object({
   CANONICAL_URL: z.string().default('http://localhost:3000'),
-  NODE_ENV: z.string().min(1),
+  NODE_ENV: z.string().optional(),
 });
 
 // Use this object to validate env file
