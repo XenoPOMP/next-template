0dd5ac02380f26ec3cad8392048715a0521dc757
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-03-31 20:06:15 +0200

    😻 update logo component

---
+++ src/components/ui/kit/Logo/Logo.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Logo } from './Logo';
+
+const meta = {
+  title: 'UI Kit / Logo',
+  component: Logo,
+  tags: ['autodoc'],
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Logo>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const sharedProps = {} satisfies Partial<Story['args']>;
+
+export const Default: Story = {
+  args: {
+    ...sharedProps,
+  },
+};
--- src/components/ui/kit/Logo/Logo.tsx
+++ src/components/ui/kit/Logo/Logo.tsx
@@ -1,7 +1,11 @@
+import cn from 'classnames';
+import { Playwrite_DE_LA } from 'next/font/google';
 import Link from 'next/link';
 import type { VariableFC } from 'xenopomp-essentials';
 
 import type { LogoProps } from './Logo.props';
+
+const font = Playwrite_DE_LA();
 
 /**
  * App`s logo component. Contains link to root
@@ -10,14 +14,16 @@ import type { LogoProps } from './Logo.props';
  */
 export const Logo: VariableFC<typeof Link, LogoProps, 'children' | 'href'> = ({
   href = '/',
+  className,
   ...props
 }) => {
   return (
     <Link
       href={href}
+      className={cn(font.className, 'lowercase', className)}
       {...props}
     >
-      Logo
+      xenopomp
     </Link>
   );
 };
