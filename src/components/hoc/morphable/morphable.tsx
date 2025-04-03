a8295bc705d0562b90a91ae119091df908ca5030
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-04-03 11:52:42 +0200

    🧪 create test

--- src/components/hoc/morphable/morphable.tsx
+++ src/components/hoc/morphable/morphable.tsx
@@ -1,5 +1,5 @@
 import type { ComponentProps, ElementType, FC } from 'react';
-import type { Undefinable } from 'xenopomp-essentials';
+import type { StrictOmit, Undefinable } from 'xenopomp-essentials';
 
 type AsGuard<Props extends Undefinable<string | number | symbol>> =
   Props extends undefined
@@ -12,23 +12,54 @@ type ExcludeNever<T> = {
   [Key in keyof T as [T[Key]] extends [never] ? never : Key]: T[Key];
 };
 
-/**
- *
- */
+type RenameField<T, K extends keyof T, Replace extends string> = {
+  [Key in keyof T as Key extends K ? Replace : Key]: T[Key];
+};
+
+type MorphlingBody<TComp extends ElementType, As> = ComponentProps<TComp> &
+  ExcludeNever<{
+    as?: As;
+  }>;
+
+type SelectiveRequired<T, K extends keyof T> = StrictOmit<T, K> &
+  Required<Pick<T, K>>;
+
+type MorphlingProps<TComp extends ElementType, As> = SelectiveRequired<
+  RenameField<MorphlingBody<TComp, As>, 'as', 'Comp'>,
+  'Comp'
+>;
+
+export function morphable<
+  TComp extends ElementType,
+  ReqProps extends Undefinable<keyof ComponentProps<TComp>>,
+  As extends AsGuard<ReqProps> = AsGuard<ReqProps>,
+>(baseComp: TComp): FC<MorphlingBody<TComp, As>>;
+
 export function morphable<
   TComp extends ElementType,
   ReqProps extends Undefinable<keyof ComponentProps<TComp>>,
   As extends AsGuard<ReqProps> = AsGuard<ReqProps>,
 >(
   baseComp: TComp,
-): FC<
-  ComponentProps<TComp> &
-    ExcludeNever<{
-      as?: As;
-    }>
-> {
+  morphling: FC<MorphlingProps<TComp, As>>,
+): FC<MorphlingBody<TComp, As>>;
+
+/** */
+export function morphable<
+  TComp extends ElementType,
+  ReqProps extends Undefinable<keyof ComponentProps<TComp>>,
+  As extends AsGuard<ReqProps> = AsGuard<ReqProps>,
+>(
+  baseComp: TComp,
+  morphling?: FC<MorphlingProps<TComp, As>>,
+): FC<MorphlingBody<TComp, As>> {
   return ({ as, ...props }) => {
     const Comp = as ?? baseComp;
+
+    if (morphling) {
+      return morphling({ Comp, ...props });
+    }
+
     return <Comp {...props} />;
   };
 }
