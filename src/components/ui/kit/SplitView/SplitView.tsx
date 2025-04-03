8da71b77a051f2ece6acd99657415ce20f77f840
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-04-03 17:06:18 +0200

    ✍️ change paddings

--- src/components/ui/kit/SplitView/SplitView.tsx
+++ src/components/ui/kit/SplitView/SplitView.tsx
@@ -10,13 +10,18 @@ import type { SplitViewProps } from './SplitView.props';
 export const SplitView = slotable<
   'div',
   ComponentProps<'div'> & SplitViewProps
->('div', ({ Comp, className, children, disableIcon, ...props }) => (
+>('div', ({ Comp, className, children, disableIcon, style, ...props }) => (
   <Comp
     className={cn(
       'flex items-center justify-between',
       'cursor-pointer',
+      'hover:bg-ui-list-fill-hover transition-colors',
       className,
     )}
+    style={{
+      padding: 'calc(var(--p-level-4) * 0.625)',
+      ...style,
+    }}
     {...props}
   >
     {children}
