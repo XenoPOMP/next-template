7be6fa5b2e5c0fbb93ae306e89827abd1371ad83
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-04-28 16:52:56 +0200

    💬 update comments

--- .config/declarations/reactcss.d.ts
+++ .config/declarations/reactcss.d.ts
@@ -2,6 +2,15 @@
 import { CSSProperties } from 'react';
 import type { Lenient } from 'xenopomp-essentials';
 
+/**
+ * Enables auto-complete feature for CSS variables
+ * inside CSSProperties objects.
+ * @example
+ * const styles: CSSProperties = {
+ *   '--value': '0',
+ *   background: 'red',
+ * };
+ */
 type CSSVariables = Record<Lenient<`--${string}`>, string>;
 
 declare module 'react' {
