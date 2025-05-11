17991f84831c66c0a63107accca82d89ce277375
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-05-11 21:12:43 +0200

    🌀 enhance types

--- src/zustand/utils/create-persistent-store.ts
+++ src/zustand/utils/create-persistent-store.ts
@@ -2,12 +2,14 @@ import type { SelectivePartial } from 'xenopomp-essentials';
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-type Creator<T> = typeof persist<T>;
+type Creator<T> = typeof persist<T, [], [], T>;
+type Init<T> = Parameters<Creator<T>>[0];
+type Options<T> = Parameters<Creator<T>>[1];
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 export const createPersistentStore = <T>(
-  creator: Parameters<Creator<T>>[0],
-  options: SelectivePartial<Parameters<Creator<T>>[1], 'storage'>,
+  creator: Init<T>,
+  options: SelectivePartial<Options<T>, 'storage'>,
 ) => {
   return create<T>()(
     persist<T>(creator, {
