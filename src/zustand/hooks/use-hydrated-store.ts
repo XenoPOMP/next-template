cbc2a04e8ee92be6f3cbd714853c955987537fb8
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-05-11 21:42:05 +0200

    ✅ create hook for handling persistent store

---
+++ src/zustand/hooks/use-hydrated-store.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import type { ReplaceReturnType } from 'xenopomp-essentials';
+
+import type { createPersistentStore } from '@/zustand/utils';
+
+type ReturnedStore<TStore> = ReturnType<typeof createPersistentStore<TStore>>;
+
+/**
+ * Handle persisted bound store.
+ *
+ * @param boundStore
+ * @param selector
+ *
+ * @example
+ * const {
+ *   state: { lang, changeLang },
+ *   isLoading,
+ * } = useHydratedStore(useSettingsStore, state => state);
+ *
+ * <>
+ *   {isLoading ? <>Loading...</> : <>Hydrated!</>}
+ * </>
+ */
+export const useHydratedStore = <TBound, TSelect = unknown>(
+  boundStore: ReturnedStore<TBound>,
+  selector: ReplaceReturnType<Parameters<ReturnedStore<TBound>>[0], TSelect>,
+) => {
+  const [hasHydrated, setHasHydrated] = useState<boolean>(false);
+  const state = boundStore(selector);
+
+  useEffect(() => {
+    if (!boundStore.persist.hasHydrated()) {
+      boundStore.persist.rehydrate();
+    }
+
+    setHasHydrated(true);
+  }, []);
+
+  return {
+    state,
+    isLoading: !hasHydrated,
+  };
+};
