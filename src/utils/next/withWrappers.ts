a4c26bfa5bc6b7e53fa9ab74cdd9eadebeb4b8c9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-05-15 11:52:30 +0200

    🌀 change type of wrapper

--- src/utils/next/withWrappers.ts
+++ src/utils/next/withWrappers.ts
@@ -1,5 +1,8 @@
 import mdxWrapper from '@next/mdx';
+import type { NextConfig } from 'next';
 import { pipe } from 'xenopomp-essentials';
+
+type NextConfigPipe = (init: NextConfig) => NextConfig;
 
 /**
  * Applies wrappers for Next.js config.
@@ -7,4 +10,4 @@ import { pipe } from 'xenopomp-essentials';
  * @example
  * export default withWrappers(nextConfig);
  */
-export const withWrappers = pipe(mdxWrapper());
+export const withWrappers: NextConfigPipe = pipe(mdxWrapper());
