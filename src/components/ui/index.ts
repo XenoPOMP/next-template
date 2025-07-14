a0b2c020a191a1121248beeb05c7687166676880
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-07-14 13:38:50 +0200

    🚮 delete prev stacks

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,8 +1,5 @@
 import Spacer from './Spacer/Spacer';
 
-export * from './Stack/Stack';
-
 export { Spacer };
-export * from './VStack/VStack';
-export * from './HStack/HStack';
+export * from './Stack/Stack';
 export * from './ZStack/ZStack';
