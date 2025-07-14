8ce3bd9843b2c131dc2804ab24506ea140ec5f2c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-07-14 14:54:56 +0200

    ✍️ update setup.vitest

--- __tests__/setup.vitest.ts
+++ __tests__/setup.vitest.ts
@@ -1,10 +1,15 @@
 import type { MatchersObject } from '@vitest/expect';
+import deepmerge from 'deepmerge';
+import * as jestExtendedMatchers from 'jest-extended';
 import { expect } from 'vitest';
 
 import { toMatchStructure } from '@test/assets/extensions';
 
-export const matchers = {
-  toMatchStructure,
-} satisfies MatchersObject;
+export const matchers = deepmerge(
+  {
+    toMatchStructure,
+  },
+  jestExtendedMatchers,
+) satisfies MatchersObject;
 
 expect.extend(matchers);
