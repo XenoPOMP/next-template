5d325ea86fa46f43150598e07ff1358c4a7a6b62
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-07-14 13:37:55 +0200

    📝 rename component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,8 @@
 import Spacer from './Spacer/Spacer';
-import Stack from './Stack/Stack';
 
-export { Stack, Spacer };
+export * from './Stack/Stack';
+
+export { Spacer };
 export * from './VStack/VStack';
 export * from './HStack/HStack';
 export * from './ZStack/ZStack';
