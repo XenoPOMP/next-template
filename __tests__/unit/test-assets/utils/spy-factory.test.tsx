396ae88cedf69f23a03ce7d6b155b4c49e0e7520
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-10-23 20:47:32 +0200

    🧪 create new spy utility

---
+++ __tests__/unit/test-assets/utils/spy-factory.test.tsx
@@ -0,0 +1,18 @@
+import { describe, test } from 'vitest';
+
+import { spyFactory } from '@test/assets/spies/spy-factory.ts';
+
+describe('spyFactory utility', () => {
+  test('Calling spy works', () => {
+    const { callSpy, expectToBeCalled, expectToBeNotCalled } =
+      spyFactory('spyFactory test');
+
+    // Call spy with some args
+    const args: any[] = [1, 'some', 'args', 2];
+    callSpy(...args);
+
+    // Run expectation func
+    expectToBeCalled(...args);
+    expectToBeNotCalled(2, 2, 7);
+  });
+});
