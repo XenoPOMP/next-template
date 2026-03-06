ce630721b5e3c19311278c6a96233840e4f7559a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-03-06 21:37:38 +0200

    🪲 fix env variables in tests

--- .config/vitest.config.ts
+++ .config/vitest.config.ts
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react';
+import type { PartialDeep } from 'type-fest';
 import tsconfigPathsPlugin from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 import { vitestExclusions } from '../.config/helpers/test-exclude';
+import type { env } from '../src/utils/env';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tsconfigPathsPlugin()],
@@ -15,6 +17,9 @@ export default defineConfig(({ mode }) => ({
       exclude: [...vitestExclusions],
     },
     setupFiles: ['./__tests__/setup.vitest.ts'],
+    env: {
+      NEXT_PUBLIC_CANONICAL_URL: 'http://localhost:3000',
+    } satisfies PartialDeep<typeof env>,
   },
   define: mode === 'test' ? {} : { global: 'window' },
 }));
