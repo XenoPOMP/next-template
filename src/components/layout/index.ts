d6135e69ebfbb2f457f85043bc6ade04be0595f2
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-03-06 21:12:22 +0200

    🚮 delete core layout component

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,6 +1,4 @@
-import CoreLayout from './CoreLayout/CoreLayout';
 import For from './For/For';
 import Providers from './Providers/Providers';
 
-// eslint-disable-next-line deprecation/deprecation
-export { Providers, CoreLayout, For };
+export { Providers, For };
