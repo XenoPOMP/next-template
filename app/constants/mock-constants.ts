ecb662e8f744cf99ad2e4e9a657aa51df03a7cb0
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-03-06 21:14:40 +0200

    🚮 delete obsolete type

--- app/constants/mock-constants.ts
+++ app/constants/mock-constants.ts
@@ -1,11 +1,6 @@
 import type { NextFont } from 'next/dist/compiled/@next/font';
 import type { Fn } from 'xenopomp-essentials';
 
-import type { GoogleFonts } from '@/types';
-
-// List of fonts that have to be mocked.
-export const FONTS_TO_MOCK: GoogleFonts[] = ['Inter', 'Geologica'];
-
 /** Default mock for any font export. */
 export const FONT_MOCK: Fn<[], NextFont> = () => ({
   className: 'mocked',
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,6 @@
 export * from './InferBoundStoreType';
 export * from './open-graph';
 export * from './todo';
-export * from './google-fonts';
 export * from './next-link-href';
 export * from './typesafe-url';
 export * from './select-keys';
