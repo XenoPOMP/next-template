608411f6b047bcfd121a086c8cea56d44a471967
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-03-12 21:11:51 +0200

    🧪 change tests

--- __tests__/unit/utilities/global-next-config.test.tsx
+++ __tests__/unit/utilities/global-next-config.test.tsx
@@ -1,35 +1,9 @@
-import type { NextConfig } from 'next';
 import { describe, expect, test } from 'vitest';
 import { assertNotThrowing } from 'xenopomp-essentials/vitest';
 
 import nextConfig from '@/utils/next';
 
 describe('Global NextConfig util', () => {
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  const testCfg = ({
-    base,
-    options,
-    expected,
-  }: {
-    base: NextConfig;
-    options: NonNullable<Parameters<typeof nextConfig>[1]>;
-    expected: NextConfig;
-  }) => {
-    const transformed = nextConfig(base, options);
-    expect(transformed).toStrictEqual(expected);
-  };
-
-  test('Wrappers can be disabled', () => {
-    testCfg({
-      base: {},
-      options: {
-        mdx: false,
-        serwist: false,
-      },
-      expected: {},
-    });
-  });
-
   test('Wrappers can be empty', () => {
     assertNotThrowing(() => nextConfig({}));
   });
@@ -44,6 +18,30 @@ describe('Global NextConfig util', () => {
             swSrc: 'app/sw.ts',
             swDest: 'public/sw.js',
           },
+        },
+      ),
+    );
+  });
+
+  test('Webpack is modified', () => {
+    const config = nextConfig({});
+    expect(config.webpack).toBeDefined();
+    assertNotThrowing(() =>
+      config.webpack!(
+        {
+          plugins: [],
+        },
+        {} as any,
+      ),
+    );
+  });
+
+  test('MDX can be disabled', () => {
+    assertNotThrowing(() =>
+      nextConfig(
+        {},
+        {
+          mdx: false,
         },
       ),
     );
