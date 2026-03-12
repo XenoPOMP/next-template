c8c48e9c1347bc061f81d51f93390dbef76a850c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-03-12 20:52:36 +0200

    🥘 apply codemods

--- app/proxy.ts
+++ app/proxy.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 import { paraglideMiddleware } from '@/paraglide/server';
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   return paraglideMiddleware(request, ({ request, locale }) => {
     request.headers.set('x-paraglide-locale', locale);
     request.headers.set('x-paraglide-request-url', request.url);
