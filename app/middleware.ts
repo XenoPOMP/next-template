f734edbe70c4f344448c400f8ea643fb9029b117
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-03-12 16:57:58 +0200

    ✍️ write middleware

---
+++ app/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+import { paraglideMiddleware } from '@/paraglide/server';
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+export function middleware(request: NextRequest) {
+  return paraglideMiddleware(request, ({ request, locale }) => {
+    request.headers.set('x-paraglide-locale', locale);
+    request.headers.set('x-paraglide-request-url', request.url);
+    return NextResponse.rewrite(request.url, request);
+  });
+}
